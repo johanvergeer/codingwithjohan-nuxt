74ef0165435536a5394de6465f50f181ee122a41
Johan Vergeer <johanvergeer@gmail.com>
2020-12-24 14:55:14 +0100

    feat[pages]: show articles list on index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,50 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">codingwithjohan-nuxt</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div
+      class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16"
+    >
+      <div class="text-4xl font-bold w-full sm:w-5/5 text-center sm:text-left">
+        <div class="leading-tight">Coding with Johan</div>
+        <div class="text-blue-700 leading-tight">Passion for Code</div>
+      </div>
+      <div class="mt-8 sm:mt-0 w-full">
+        <!--      <g-image src="~/img/illustrations/programming.png" alt="programming" class="mx-auto sm:mx-0"/>-->
       </div>
     </div>
+
+    <div class="container-inner mx-auto">
+      <p class="text-lg sm:text-xl">
+        Hi, my name is Johan. I'm a full stack software engineer and code
+        enthusiast. I'm a fan of sharing knowledge, learning a little bit
+        everyday and teaching that knowledge to others. On my day job I work
+        within agile teams to build applications using Python.
+      </p>
+
+      <div class="flex justify-between items-center py-6">
+        <div class="w-full sm:w-1/2 px-8 py-8 sm:py-0">
+          <!--        <g-image src="~/img/illustrations/teaching.png" alt="source-code"/>-->
+        </div>
+        <div class="w-1/2 px-8 hidden sm:flex justify-end">
+          <!--        <g-image src="~/img/illustrations/source-code.png" alt="source-code"/>-->
+        </div>
+      </div>
+    </div>
+    <articles-list :articles="articles" />
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script>
+import ArticlesList from '~/components/blog/ArticlesList'
 
-export default Vue.extend({})
+export default {
+  components: { ArticlesList },
+  async asyncData({ $content, params }) {
+    const articles = await $content('blog', params.slug)
+      .only(['title', 'slug', 'description', 'createdAt', 'body'])
+      .sortBy('createdAt', 'asc')
+      .fetch()
+
+    return { articles }
+  },
+}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
