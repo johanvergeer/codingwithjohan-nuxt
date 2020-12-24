977eaa103ac23a1417e6b2b60a60c78c51f44cad
Johan Vergeer <johanvergeer@gmail.com>
2020-12-24 14:48:07 +0100

    create component to calculate the time to read of an article

---
+++ components/blog/ReadingTime.vue
@@ -0,0 +1,21 @@
+<template>
+  <span>{{ readingTime() }} min read</span>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Article } from '~/types/Article'
+
+@Component
+export default class ReadingTime extends Vue {
+  @Prop() private article?: Article
+
+  readingTime() {
+    const contentString = JSON.stringify(this.article?.body)
+    const words = contentString.split(' ').length
+    const wordsPerMinute = 200
+
+    return Math.ceil(words / wordsPerMinute)
+  }
+}
+</script>
---
+++ types/Article.ts
@@ -0,0 +1,7 @@
+export interface Article {
+  title: string
+  slug: string
+  description: string
+  createdAt: string
+  body: string
+}
