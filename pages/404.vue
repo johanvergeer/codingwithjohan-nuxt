b49994cf71a2385957fa8c901f4d368779e86f38
Johan Vergeer <johanvergeer@gmail.com>
2020-12-25 10:27:30 +0100

    feat[error-handling]: create 404 page

---
+++ pages/404.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container-inner mx-auto py-16">
+    <h1 class="mb-16 text-center">Page Not Found</h1>
+    <div class="flex justify-center">
+      <img src="~/assets/img/illustrations/not-found.svg" alt="Not Found">
+    </div>
+  </div>
+</template>
