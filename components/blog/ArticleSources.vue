55f45731cf0f130239026eb3844fd7b3ff9d6353
Johan Vergeer <johanvergeer@gmail.com>
2020-12-25 16:03:10 +0100

    feat[articles]: create component to view article sources

---
+++ components/blog/ArticleSources.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="article.sources && article.sources.length > 0">
+    <h1>Sources</h1>
+    <div v-for="source in article.sources" :key="source.title">
+      <a :href="source.url" target="_blank">{{ source.title }}</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Article } from '~/types/Article'
+
+@Component
+export default class ArticleTags extends Vue {
+  @Prop() private article?: Article
+}
+</script>
--- types/Article.ts
+++ types/Article.ts
@@ -1,3 +1,8 @@
+interface ArticleSource {
+  title: string
+  url: string
+}
+
 export interface Article {
   title: string
   slug: string
@@ -5,4 +10,5 @@ export interface Article {
   createdAt: string
   body: string
   featureImage?: string
+  sources?: [ArticleSource]
 }
