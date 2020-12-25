b777489a4edc3780890567c2a31c2b8b028d0cba
Johan Vergeer <johanvergeer@gmail.com>
2020-12-25 15:02:06 +0100

    feat[articles]: create component to display tags

---
+++ components/blog/ArticleTags.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nuxt-link
+      v-for="tag in article.tags"
+      :key="tag"
+      :to="{ name: 'blog-tag-tag', params: { tag: tag } }"
+      class="bg-gray-300 rounded-full px-4 py-2 mr-4 hover:bg-blue-300"
+    >
+      {{ tag }}
+    </nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Article } from '~/types/Article'
+
+@Component
+export default class ArticleTags extends Vue {
+  @Prop() private article?: Article
+}
+</script>
