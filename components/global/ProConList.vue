ab87c8220132321864b4a381008063fca6e81a25
Johan Vergeer <johanvergeer@gmail.com>
2020-12-26 11:43:15 +0100

    feat[articles]: create component to create lists with pro's and con's

---
+++ components/global/ProConList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h4 v-if="title">{{ title }}</h4>
+    <div
+      class="grid"
+      :class="[`md:grid-cols-${columns}`, columns > 1 ? 'gap-4' : '']"
+    >
+      <ul
+        class="procon list-none list-outside"
+        :class="[columns > 1 ? '' : 'procon-list-mb']"
+      >
+        <li v-for="pro in pros" class="flex">
+          <div class="mr-5"><icon-plus /></div>
+          <div>{{ pro }}</div>
+        </li>
+      </ul>
+      <ul class="procon list-none list-outside">
+        <li v-for="con in cons" class="flex">
+          <div class="mr-5"><icon-minus /></div>
+          <div>{{ con }}</div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import IconPlus from '~/components/atoms/icons/IconPlus.vue'
+import IconMinus from '~/components/atoms/icons/IconMinus.vue'
+@Component({
+  components: { IconMinus, IconPlus },
+})
+export default class ProConList extends Vue {
+  @Prop({ default: 2 }) private columns?: number
+  @Prop({ default: "Pro's and Con's" }) private title?: string
+  @Prop() private pros?: [String]
+  @Prop() private cons?: [String]
+}
+</script>
+
+<style scoped lang="scss">
+.procon {
+  padding-left: 0;
+}
+
+.procon-list-mb {
+  @apply mb-5;
+}
+</style>
