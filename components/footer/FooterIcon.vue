f995dfbf93403f08df72bb385a57dd08b9257b00
Johan Vergeer <johanvergeer@gmail.com>
2020-12-26 14:08:36 +0100

    fix[footer]: footer icon links now work

--- components/footer/FooterIcon.vue
+++ components/footer/FooterIcon.vue
@@ -1,14 +1,13 @@
 <template>
   <li>
-    <a target="_blank" class="text-white hover:text-gray-400" @href="href">
-      <img :src="require(`~/assets/img/icons/${img}.png`)" @alt="img" />
+    <a target="_blank" class="text-white hover:text-gray-400" :href="href">
+      <img :src="require(`~/assets/img/icons/${img}.png`)" :alt="img" />
     </a>
   </li>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
-
 @Component
 export default class ProConList extends Vue {
   @Prop() private href?: [String]
