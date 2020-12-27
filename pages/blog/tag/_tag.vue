2acabf774cc6b9c827fe60b61155bd3370d19b52
Johan Vergeer <johanvergeer@gmail.com>
2020-12-27 15:01:41 +0100

    refactor[pages]: use typescript for tag page

--- pages/blog/tag/_tag.vue
+++ pages/blog/tag/_tag.vue
@@ -2,36 +2,51 @@
   <div
     class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16"
   >
-    <div class="container-inner mx-auto py-16">
-      <h1 class="border-gray-700 border-b mb-12">Tag: {{ tag }}</h1>
-      <div>Articles: {{ articles }}</div>
+    <p v-if="$fetchState.pending">
+      Loading articles for tag {{ $route.params.tag }}...
+    </p>
+    <p v-else-if="$fetchState.error">Error while loading the tags!</p>
+    <div v-else class="container-inner mx-auto py-16">
+      <h1 class="border-gray-700 border-b mb-12">
+        Tag: {{ $route.params.tag }}
+      </h1>
       <articles-list :articles="articles" />
     </div>
   </div>
 </template>
 
-<script>
-import ArticlesList from '~/components/blog/ArticlesList'
+<script lang="ts">
+import { IContentDocument } from '@nuxt/content/types/content'
+import { Component, Vue } from 'nuxt-property-decorator'
+import ArticleMeta from '~/components/blog/ArticleMeta.vue'
+import ArticleSeries from '~/components/blog/ArticleSeries.vue'
+import ArticlesList from '~/components/blog/ArticlesList.vue'
+import ArticleSources from '~/components/blog/ArticleSources.vue'
+import ArticleTags from '~/components/blog/ArticleTags.vue'
+import FeatureImage from '~/components/blog/FeatureImage.vue'
 
-export default {
-  components: { ArticlesList },
-  async asyncData({ $content, params, redirect }) {
-    const tag = params.tag
+@Component({
+  components: {
+    ArticleMeta,
+    ArticleTags,
+    FeatureImage,
+    ArticleSeries,
+    ArticleSources,
+    ArticlesList,
+  },
+})
+export default class Tag extends Vue {
+  private articles: IContentDocument | IContentDocument[] = []
 
-    const articles = await $content('blog', params.slug)
-      .where({ tags: { $contains: tag } })
-      // .where(function (article) {
-      //   return article.tags
-      //     .map((tag) => tag.toLowerCase())
-      //     .contains(params.tag.toLowerCase())
-      // })
+  async fetch() {
+    this.articles = await this.$nuxt
+      .$content('blog', this.$route.params.slug)
+      .where({ tags: { $contains: this.$route.params.tag } })
       .only(['title', 'slug', 'description', 'createdAt', 'body'])
       .sortBy('createdAt', 'asc')
       .fetch()
 
-    if (articles.length > 0) return { articles, tag }
-
-    return redirect('/404')
-  },
+    if (this.articles.length < 1) return this.$nuxt.context.redirect('/404')
+  }
 }
 </script>
