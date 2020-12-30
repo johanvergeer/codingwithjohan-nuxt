687a11adb0fab182ad58b5af8dec240966ba781c
Johan Vergeer <johanvergeer@gmail.com>
2020-12-30 18:48:32 +0100

    fix: next button should be aligned to the right side when prev button is hidden

--- components/blog/ArticleSeriesPrevNext.vue
+++ components/blog/ArticleSeriesPrevNext.vue
@@ -6,9 +6,8 @@
     Error occurred while checking for other articles in the series...
   </div>
   <div v-else-if="document.series" class="md:flex md:justify-between">
-    <div class="md:flex-1">
+    <div v-if="prev" class="md:w-2/5">
       <nuxt-link
-        v-if="prev"
         :to="{ name: 'blog-slug', params: { slug: prev.slug } }"
         class="flex border border-blue-100 rounded-md items-center p-2 pr-5"
       >
@@ -16,11 +15,10 @@
         <div class="text-2xl ml-5">{{ prev.title }}</div>
       </nuxt-link>
     </div>
-    <div class="md:flex-none md:w-1/12 invisible md:visible"></div>
-    <div class="md:flex-1 mt-10 md:mt-0">
+    <div></div>
+    <div v-if="next" class="md:w-2/5 mt-10 md:mt-0">
       <nuxt-link
-        v-if="prev"
-        :to="{ name: 'blog-slug', params: { slug: prev.slug } }"
+        :to="{ name: 'blog-slug', params: { slug: next.slug } }"
         class="flex border border-blue-100 rounded-md items-center p-2 pl-5 justify-end"
       >
         <div class="text-2xl mr-5">{{ next.title }}</div>
