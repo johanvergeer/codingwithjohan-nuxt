8186807b0daf279e9ca65ca48c04ab265009786c
Johan Vergeer <johanvergeer@gmail.com>
2021-01-15 17:34:37 +0100

    chore: configure commitlint type-enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'chore',
+        'refactor',
+        'test',
+        'revert',
+        'content', // For articles, tutorials, etc...
+      ],
+    ],
+  },
 }
