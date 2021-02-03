a9cd044aafc4b48d624e9c3e0562ab3f1f7efe35
Johan Vergeer <johanvergeer@gmail.com>
2021-02-03 21:00:20 +0100

    fix(components): add empty div used in case the article does not have a feature image

--- components/blog/FeatureImage.vue
+++ components/blog/FeatureImage.vue
@@ -2,6 +2,7 @@
   <div v-if="article.featureImage">
     <img :src="article.featureImage" alt="Feature Image" />
   </div>
+  <div v-else><!-- No feature image available--></div>
 </template>
 
 <script lang="ts">
