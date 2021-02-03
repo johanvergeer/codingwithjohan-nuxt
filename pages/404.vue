cd79267c8fd1b00b2bd957082b74e5f094064569
Johan Vergeer <johanvergeer@gmail.com>
2021-02-03 21:07:58 +0100

    fix(pages): remove old 404 page

--- pages/404.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="container-inner mx-auto py-16">
-    <h1 class="mb-16 text-center">Page Not Found</h1>
-    <div class="flex justify-center">
-      <img src="~/assets/img/illustrations/not-found.svg" alt="Not Found" />
-    </div>
-  </div>
-</template>
