1250e72205bc8a46baefa959bf3277a30b7ec927
Johan Vergeer <johanvergeer@gmail.com>
2022-01-08 13:48:12 +0100

    chore: upgrade stylelint to v14

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-recommended-vue/scss',
+    'stylelint-config-prettier',
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {},
